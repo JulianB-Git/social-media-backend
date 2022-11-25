fb754c637fb7c34fb89e751e970e86c9bb6008c8
DESKTOP-GE411VK\julia <julian.benade@gmail.com>
2022-11-25 10:31:11 +0200

    Add health api route to the security permitted routes

--- src/main/java/com/social/appbackend/controllers/HealthController.java
+++ src/main/java/com/social/appbackend/controllers/HealthController.java
@@ -11,7 +11,7 @@ public class HealthController {
 
     @GetMapping
     public ResponseEntity<?> getHealth(){
-        return ResponseEntity.ok().body("Health");
+        return ResponseEntity.ok().body("Healthy");
     }
 
 }
--- src/main/java/com/social/appbackend/security/WebSecurityConfig.java
+++ src/main/java/com/social/appbackend/security/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/auth/**").permitAll()
+                .authorizeRequests().antMatchers("/auth/**", "/health").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
